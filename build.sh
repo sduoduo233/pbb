5588bc7e4302f6b1fde18204caa5f4519bb925e8
duoduo <sduoduo233@gmail.com>
2025-06-28 16:27:34 +0800

    strip

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 export CGO_ENABLED=1
 
-go build -ldflags '-linkmode external -extldflags "-static"' -tags sqlite_omit_load_extension -o bin/hub -trimpath -buildvcs=false .
-go build -ldflags '-linkmode external -extldflags "-static"' -tags sqlite_omit_load_extension -o bin/agent -trimpath -buildvcs=false ./agent
+go build -ldflags '-s -w -linkmode external -extldflags "-static"' -tags sqlite_omit_load_extension -o bin/hub -trimpath -buildvcs=false .
+go build -ldflags '-s -w -linkmode external -extldflags "-static"' -tags sqlite_omit_load_extension -o bin/agent -trimpath -buildvcs=false ./agent
 
 chown -R 1000:1000 ./bin
