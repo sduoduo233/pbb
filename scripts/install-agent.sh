40f2d05aaa235793cab1a6b98d6df5fd67dd0b82
duoduo <sduoduo233@gmail.com>
2025-06-29 21:42:50 +0800

    install script

---
+++ scripts/install-agent.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
+uname=$(uname -m)
+case $uname in
+    x86_64)
+        arch="amd64"
+        ;;
+    aarch64|arm64)
+        arch="arm64"
+        ;;
+    i386|i686)
+        arch="386"
+        ;;
+    *arm*)
+        arch="arm32-v7a"
+        ;;
+    *)
+        echo "Unsupported architecture: $uname"
+        exit 1
+        ;;
+esac
+
+mkdir -p /opt/pbb
+$binary_url="https://dl.exec.li/agent-$arch"
+echo "> Downloading $binary_url to /opt/pbb/agent"
+curl -L "$binary_url" > /opt/pbb/agent
+
+echo "> Setting permissions for /opt/pbb/agent"
+chmod +x /opt/pbb/agent
+
+is_systemd=$(command -v systemctl)
+is_openrc=$(command -v rc-status)
+
+if [ "$is_systemd" ]; then
+
+    echo "> Creating systemd service file at /etc/systemd/system/pbb-agent.service"
+    cat <<EOF > /etc/systemd/system/pbb-agent.service
+[Unit]
+Description=PBB Agent Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/opt/pbb/agent
+Restart=on-failure
+User=nobody
+WorkingDirectory=/opt/pbb
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    echo "> Starting systemd service"
+    systemctl daemon-reload
+    systemctl enable pbb-agent.service
+    systemctl start pbb-agent.service
+
+elif [ "$is_openrc" ]; then
+
+    echo "> Creating OpenRC service file at /etc/init.d/pbb-agent"
+    cat <<EOF > /etc/init.d/pbb-agent
+#!/sbin/openrc-run
+description="PBB Agent Service"
+command="/opt/pbb/agent"
+directory="/opt/pbb"
+depend() {
+    need net
+}
+
+EOF
+
+    echo "> Starting OpenRC service"
+
+    chmod +x /etc/init.d/pbb-agent
+    rc-update add pbb-agent default
+
+    /etc/init.d/pbb-agent start
+
+else
+    echo "Neither systemd nor OpenRC found. Please create a service file manually."
+fi
+
+echo "> PBB agent installation complete."
---
+++ scripts/install-hub.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
+uname=$(uname -m)
+case $uname in
+    x86_64)
+        arch="amd64"
+        ;;
+    aarch64|arm64)
+        arch="arm64"
+        ;;
+    *)
+        echo "Unsupported architecture: $uname"
+        exit 1
+        ;;
+esac
+
+mkdir -p /opt/pbb
+$binary_url="https://dl.exec.li/hub-$arch"
+echo "> Downloading $binary_url to /opt/pbb/hub"
+curl -L "$binary_url" > /opt/pbb/hub
+
+echo "> Setting permissions for /opt/pbb/hub"
+chmod +x /opt/pbb/hub
+
+is_systemd=$(command -v systemctl)
+is_openrc=$(command -v rc-status)
+
+if [ "$is_systemd" ]; then
+
+    echo "> Creating systemd service file at /etc/systemd/system/pbb-hub.service"
+    cat <<EOF > /etc/systemd/system/pbb-hub.service
+[Unit]
+Description=PBB Hub Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/opt/pbb/hub
+Restart=on-failure
+User=root
+WorkingDirectory=/opt/pbb
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    echo "> Starting systemd service"
+    systemctl daemon-reload
+    systemctl enable pbb-hub.service
+    systemctl start pbb-hub.service
+
+elif [ "$is_openrc" ]; then
+
+    echo "> Creating OpenRC service file at /etc/init.d/pbb-hub"
+    cat <<EOF > /etc/init.d/pbb-hub
+#!/sbin/openrc-run
+description="PBB Hub Service"
+command="/opt/pbb/hub"
+directory="/opt/pbb"
+depend() {
+    need net
+}
+
+EOF
+
+    echo "> Starting OpenRC service"
+
+    chmod +x /etc/init.d/pbb-hub
+    rc-update add pbb-hub default
+
+    /etc/init.d/pbb-hub start
+
+else
+    echo "Neither systemd nor OpenRC found. Please create a service file manually."
+fi
+
+echo "> PBB Hub installation complete."
