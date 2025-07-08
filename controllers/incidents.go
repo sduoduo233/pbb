d85e257fc8035b1675461bdfffec9e030d5df0c8
duoduo <sduoduo233@gmail.com>
2025-07-08 16:33:23 +0800

    handle non existing incident

--- controllers/incidents.go
+++ controllers/incidents.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 
 	"github.com/labstack/echo/v4"
@@ -17,6 +19,9 @@ func viewIncidents(c echo.Context) error {
 		var incident db.IncidentWithServerLabel
 		err := db.DB.Get(&incident, "SELECT incidents.id, servers.label AS server_label, incidents.server_id, incidents.started_at, incidents.ended_at, incidents.state, servers.hidden AS hidden FROM incidents LEFT JOIN servers ON incidents.server_id = servers.id WHERE incidents.id = ?", id)
 		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				continue
+			}
 			return fmt.Errorf("db: %w", err)
 		}
 		if !incident.Hidden || user != nil {
